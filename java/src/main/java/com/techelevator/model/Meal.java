a0dd938a8696644fa1aaace5e0ca9dd8d5827e58
Dan Halsey <dvhalsey42@gmail.com>
2022-09-07 12:17:26 -0400

    Created meal plan model, updated schema to include correct meal_plan table

--- java/src/main/java/com/techelevator/model/Meal.java
+++ java/src/main/java/com/techelevator/model/Meal.java
@@ -18,23 +18,18 @@ public class Meal {
     public int getMeal_id() {
         return meal_id;
     }
-
     public void setMeal_id(int meal_id) {
         this.meal_id = meal_id;
     }
-
     public String getMeal_name() {
         return meal_name;
     }
-
     public void setMeal_name(String meal_name) {
         this.meal_name = meal_name;
     }
-
     public int getUser_id() {
         return user_id;
     }
-
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
---
+++ java/src/main/java/com/techelevator/model/MealPlan.java
@@ -0,0 +1,53 @@
+package com.techelevator.model;
+
+public class MealPlan {
+
+
+    private int plan_id;
+    private String meal_plan_name;
+    private int user_id;
+
+
+    public MealPlan(){};
+
+    public MealPlan(int plan_id, String meal_plan_name, int user_id) {
+        this.plan_id = plan_id;
+        this.meal_plan_name = meal_plan_name;
+        this.user_id = user_id;
+    }
+
+
+    public int getPlan_id() {
+        return plan_id;
+    }
+
+    public void setPlan_id(int plan_id) {
+        this.plan_id = plan_id;
+    }
+
+    public String getMeal_plan_name() {
+        return meal_plan_name;
+    }
+
+    public void setMeal_plan_name(String meal_plan_name) {
+        this.meal_plan_name = meal_plan_name;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    @Override
+    public String toString() {
+        return "MealPlan{" +
+                "plan_id=" + plan_id +
+                ", meal_plan_name='" + meal_plan_name + '\'' +
+                ", user_id=" + user_id +
+                '}';
+    }
+
+}
